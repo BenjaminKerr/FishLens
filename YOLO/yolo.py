a050cc70270ef63d5f3f447dffda60c121169256
Aden Ratliff <aden.ratliff@oit.edu>
2025-11-20 20:44:49 -0800

    Added sample fish video for analysis; Restructured and reformatted yolo.py.

--- YOLO/yolo.py
+++ YOLO/yolo.py
@@ -1,11 +1,33 @@
+#------------------------------------------------------------------
+# Name: YOLOv8 Image Analysis Script
+# Authors: Aden Ratliff; Neil
+# Last Edited: 11.20.2025
+# Summary and Notes:
+# This script analyses videos to detect the probability of their
+# containing a fish using YOLOv8.
+#
+# Script currently uses a sample stock video for testing purposes.
+# Sample video is incorrectly identified as multiple objects, but
+# most often as a bird.
+#------------------------------------------------------------------
+
+# Import any nessecary libraries
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt") 
 # Standard YOLOv8 model
+model = YOLO("yolov8n.pt") 
 
-results = model.predict(source="https://ultralytics.com/images/bus.jpg", show=True, save=True, project="image_results", name="trial") 
-# Analyzed images are exported into 'image_results/trial' with trial incrementing with each execution (e.g., trial1, trial2, etc.)
 
-results[0].plot()
+# Input folder: sample_images
+# Output folder: results/trial1, where trial1 is auto-incremented
+# each time the program is run.
+results = model.predict(
+    source="sample_images/",
+    show=True, 
+    save=True, 
+    project="results", 
+    name="trial"
+) 
+
 # Displays image with bounding boxes
-
+results[0].plot()
