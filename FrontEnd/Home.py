9a08f564bec2a635b98e0153bde203f765683a22
Benjamin Kerr <benjamin.kerr@oit.edu>
2025-11-22 15:02:14 -0800

    Vids downloaded from main

--- FrontEnd/Home.py
+++ FrontEnd/Home.py
@@ -1,6 +1,7 @@
 # APP.PY: FishLens - A Streamlit frontend for the FishLens fish identifier
 
 # Import necessary libraries
+import os
 import streamlit as st # Streamlit for web app
 import pandas as pd # Pandas for data manipulation
 import numpy as np # NumPy for numerical operations
@@ -19,6 +20,9 @@ if not st.session_state.vids: # Initial upload
     vids = st.file_uploader("", accept_multiple_files=True, type=["mp4", "asf"])
     if vids:
         st.session_state.vids = list(vids)
+        for vid in vids:
+            with open(os.path.join("SavedVids",vid.name),"wb") as f:
+                f.write(vid.getbuffer())
         st.rerun()
 else: # Subsequent uploads
     vids = st.session_state.vids
