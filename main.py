96c14fcd7ebdb1c800f3adb450ad3aab19b542cc
Aden Ratliff <aden.ratliff@oit.edu>
2025-12-04 00:48:13 -0800

    Fixed bug created by previous commit. Added clarification to main.py.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# This main represents the minimum integration that will be needed in the actual main.
+# File path is passed from FE as argv[1]. 
+# File name and path are needed seperately for different parts of YOLO function.
 import os
 from YOLO.yolo import run_yolo
 
@@ -7,4 +10,4 @@ else:
     FILEPATH = "sample_data/"
 
 for file in os.listdir(FILEPATH):
-    run_yolo(file, FILEPATH + file)
+    run_yolo(file, FILEPATH + "\\" + file)
