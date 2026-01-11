3b9f08811933e9c394e6a2df0277c3794e57cb29
Kaharra <Kaharraw81505@gmail.com>
2026-01-11 15:56:31 -0800

    New Signin Method without username/password check

--- Upgraded-App/SigninPage.xaml.cs
+++ Upgraded-App/SigninPage.xaml.cs
@@ -23,55 +23,33 @@ namespace FishLens_App
     /// </summary>
     public partial class SigninPage : Window
     {
-        private const string CLIENT_ID = "aed2d2f8-02e0-413c-a365-cdf377f5c329";
-        private const string AUTHORITY = "https://login.microsoftonline.com/common";
-        private string[] SCOPES = new[] { "User.Read", "Files.ReadWrite" };
-
-        // Create client
-        private IPublicClientApplication app =
-            PublicClientApplicationBuilder.Create(CLIENT_ID)
-                .WithAuthority(AUTHORITY)
-                .WithRedirectUri("http://localhost")  
-                .Build();
+    
         public SigninPage()
         {
             InitializeComponent();
         }
 
-        public async Task<AuthenticationResult> Signin()
+        public bool Signin(string username, string password)
         {
-            try
+            bool SigninSuccessful = false;
+          //Temp logic below until create database for logins
+            if (username == null || password == null)
             {
-                var result = await app
-                    .AcquireTokenInteractive(SCOPES)
-                    .WithPrompt(Prompt.SelectAccount)
-                    .ExecuteAsync();
-
-                // Return the clients access token
-                return result;
+                SigninSuccessful = true;
             }
 
-            catch (MsalException ex)
-            {
-                MessageBox.Show($"Login failed: {ex.Message}");
-                return null;
-            }
+            return SigninSuccessful;
         }
 
-        //private async void signinbutton(object sender, RoutedEventArgs e)
-        //{
-        //    var result = await Signin();
-        //    if(result != null) {
-        //        MessageBox.Show("Sign in successful!");
-        //        MainWindow main = new MainWindow(app, result);
-        //        main.Show();
-        //        this.Close();
+        public void SigninButtonClick(object sender, RoutedEventArgs e)
+        {
+            bool Status;
 
-        //    }
-        //    else
-        //        MessageBox.Show("Sign in unsuccesful, retry");
-
-        //}
+            String username = UserName.Text;
+            
+            String password = PassWord.ToString();
+            Status = Signin(username, password);
+        }
     
     }
 }
