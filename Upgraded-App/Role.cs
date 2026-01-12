f59b3a8681dbab71dbe227f2e20eee0ad4617201
Kaharra <Kaharraw81505@gmail.com>
2026-01-11 17:04:15 -0800

    Added User and Role classes as well as a Permissions enum. Nothing is currently set up to happen with them.

---
+++ Upgraded-App/Role.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Created by: Kaharra Wilcoxon
+// Created on: 1/11/2025
+// Class Description:
+// Role is the class of all roles like Admin or User for our Users to allow Admin access to their Employees and other settings/history that not all users/employees need access to
+
+namespace FishLens_App
+{
+    public enum Permission
+    {
+        Settings,
+        History,
+        UserSettings,
+        CreateRole,
+        CreateUser
+    }
+
+    class Role
+    {
+        public string Name { get; set; }
+        public List<Permission> Permissions { get; set; }
+
+        public Role(string name, List<Permission> permissions)
+        {
+            Name = name;
+            Permissions = permissions;
+        }
+    }
+}
---
+++ Upgraded-App/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishLens_App
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role role { get; set; }
+
+        public User(string username, string pass, Role Role)
+        {
+            Username = username;
+            Password = pass;
+            Role = role;
+        }
+
+
+        
+
+    }
+}
