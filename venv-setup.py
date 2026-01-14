359065d2f3a12a93b3a8678916607479aca65df7
Aden Ratliff <aden.ratliff@oit.edu>
2026-01-14 00:30:45 -0800

    Changes to venv setup instructions and .gitignore based on further testing.

---
+++ venv-setup.py
@@ -0,0 +1,45 @@
+# ****************************************************************
+# File: venv-setup.py
+# Description: Setup script for virtual environment.
+# Notes: This file will eventually be automated via a batch file
+# or something, but for now it's manual. This file MUST BE RAN
+# before testing any other code.
+# 
+# #########################
+# INSTALLATION INSTRUCTIONS
+# #########################
+# 1. Ensure you have Python 3.12.10 installed at FishLens/Python.
+# 2. In the Active Interpreter selection in VS Code, navigate to the dedicated Python executable:
+#    Python\python.exe
+# 3. Run this script to create a virtual environment and install dependencies.
+# 4. After script finishes, run the following command in VS Code Terminal:
+# Python\venv\Scripts\activate
+# 5. In the Active Interpreter selection, navigate to the venv Python executable:
+#    Python\venv\Scripts\python.exe
+# 6. Run trainer.py as well to create the FSI model.
+# ****************************************************************
+
+import sys
+import subprocess
+import os
+
+# Initialize paths
+project_root = os.path.dirname(os.path.abspath(__file__))
+python_path = os.path.join(project_root, 'Python')
+venv_path = os.path.join(python_path, 'venv')
+requirements_path = os.path.join(project_root, 'requirements.txt')
+
+# Create venv path if it doesn't exist
+if not os.path.exists(venv_path):
+    print(f"Creating venv folder...")
+    subprocess.run([os.path.join(python_path, "python.exe"), "-m", "venv", venv_path], check=True)
+    print(f"venv folder created.")
+else:
+    print(f"Virtual environment folder already exists--skipping creation.")
+
+# Install all dependencies from requirements.txt
+pip_exe = os.path.join(venv_path, 'Scripts', 'pip.exe')
+print(f"Installing dependencies from requirements.txt...")
+subprocess.run([pip_exe, "install", "--upgrade", "-r", requirements_path], check=True)
+print("All dependencies installed.")
+print("Setup complete.")
