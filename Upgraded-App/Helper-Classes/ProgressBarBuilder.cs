9a1511c977479a80d160442d9d6c5ea095abb715
Aden Ratliff <aden.ratliff@oit.edu>
2026-05-01 00:07:51 -0700

    Refactored progress bar with animated "in-progress" state and complete state for videos.

--- Upgraded-App/Helper-Classes/ProgressBarBuilder.cs
+++ Upgraded-App/Helper-Classes/ProgressBarBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Org.BouncyCastle.Security;
+﻿using DocumentFormat.OpenXml.Math;
+using Org.BouncyCastle.Security;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 
-namespace FishLens_App.Helper_Classes
+namespace FishLens_App
 {
     class ProgressBarBuilder
     {
@@ -15,14 +16,21 @@ namespace FishLens_App.Helper_Classes
         // Function: Build
         // Description: Builds a new row of bars.
         // **************************************************
-            public List<bool> Build(int count, int active)
+        public List<MainWindow.VideoProgressState> Build(int count, int currentIndex)
+        {
+            var result = new List<MainWindow.VideoProgressState>();
+
+            for (int i = 0; i < count; i++)
             {
-                var result = new List<bool>();
-
-                for (int i = 0; i < count; i++)
-                    result.Add(i < active);
-
-                return result;
+                if (i < currentIndex)
+                    result.Add(MainWindow.VideoProgressState.Filled);
+                else if (i == currentIndex)
+                    result.Add(MainWindow.VideoProgressState.Active);
+                else
+                    result.Add(MainWindow.VideoProgressState.Empty);
             }
+
+            return result;
+        }
     }
 }
