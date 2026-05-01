595427b5a64c340d28440749fc39402be718392b
Aden Ratliff <aden.ratliff@oit.edu>
2026-04-30 23:27:53 -0700

    Progress Bar Builder delegated to dedicated helper class.

---
+++ Upgraded-App/Helper-Classes/ProgressBarBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace FishLens_App.Helper_Classes
+{
+    class ProgressBarBuilder
+    {
+        // **************************************************
+        // Function: Build
+        // Description: Builds a new row of bars.
+        // **************************************************
+            public List<bool> Build(int count, int active)
+            {
+                var result = new List<bool>();
+
+                for (int i = 0; i < count; i++)
+                    result.Add(i < active);
+
+                return result;
+            }
+    }
+}
--- Upgraded-App/Pages/MainWindow.xaml.cs
+++ Upgraded-App/Pages/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 using System.Collections.ObjectModel;
+using FishLens_App.Helper_Classes;
 
 namespace FishLens_App
 {
@@ -85,17 +86,18 @@ namespace FishLens_App
 
         // Video player state
         private DispatcherTimer _videoTimer;
+        private ProgressBarBuilder _builder = new ProgressBarBuilder();
         private bool _isDraggingScrubber = false;
         private bool _isPlaying = false;
         private int _suppressTimerTicks = 0;
         private string _playbackTempPath = null; // temp MP4 created from ASF for accurate scrubbing
         private bool _processingComplete = false;
+        public ObservableCollection<bool> Bars { get; } = new ObservableCollection<bool>();
 
         // Multi-track state - all tracks for the currently displayed video
         private List<FishLens_App.Models.Video> _currentTracks = new List<FishLens_App.Models.Video>();
         private int _currentTrackIndex;
 
-        public ObservableCollection<bool> Bars { get; } = new ObservableCollection<bool>();
 
         #endregion
 
@@ -1065,12 +1067,13 @@ namespace FishLens_App
                 {
                     _totalVideos = total;
 
-                    Build(_totalVideos, 0);
                     Dispatcher.Invoke(() =>
                     {
-                        //analysisProgressBar.Minimum = 0;
-                        //analysisProgressBar.Maximum = total;
-                        //analysisProgressBar.Value = 0;
+                        Bars.Clear();
+                        foreach (var b in _builder.Build(_totalVideos, 0))
+                        {
+                            Bars.Add(b);
+                        }
                     });
                 }
                 else if (line.StartsWith("[PROGRESS] VIDEO:"))
@@ -3282,19 +3285,6 @@ namespace FishLens_App
 
         }
 
-        // **************************************************
-        // Function: Build
-        // Description: Builds a new row of bars.
-        // **************************************************
-        public void Build(int count, int active)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                Bars.Clear();
 
-                for (int i = 0; i < count; i++)
-                    Bars.Add(i < active);
-            });
-        }
     }
 }
